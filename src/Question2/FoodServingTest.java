9b0937006775663a03841a24f4b0cfaf22db8887
sorowar230 <sorowar230@gmail.com>
2018-02-13 18:16:12 -0500

    Changes on FoodServingTest.class and FoodServingTest.java

--- src/Question2/FoodServingTest.java
+++ src/Question2/FoodServingTest.java
@@ -1,6 +1,5 @@
 package Question2;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,7 +21,7 @@ public class FoodServingTest extends TestBase {
 	}
 	
 	
-	//@Test(priority=1)
+	@Test(priority=1)
 	public void verify_displayItems() {
 		String foodTitles[] = foodservingpage.display3rd_5th_Item();
 		for (String item: foodTitles) {
